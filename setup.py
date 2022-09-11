81451b22875e07cae04e80fbb2137e8794c86ff2
Leo <leonhard.hesse@outlook.com>
2022-09-12 00:46:49 +0200

    renamed for pip

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='HeRo.KY040',
+   name='HeRo_KY040',
    version='1.0',
    description='Library for encoder KY-040',
    author='Leonhard Hesse',
    author_email='service@hero-devs.de',
-   packages=['HeRo.KY040'],
+   packages=['HeRo_KY040'],
    install_requires=['RPi.GPIO'], 
 )
