cc1066180bb3ef86302b2694446a3389c6899ac2
Leo <leonhard.hesse@outlook.com>
2022-09-11 21:11:22 +0200

    test button

--- HeRo_KY040.py
+++ HeRo_KY040.py
@@ -17,6 +17,7 @@ class Encoder:
 
     BOUNCETIME_BUTTON_MS = 300
     ROT_ENC_TABLE = [0,1,1,0,1,0,0,1,1,0,0,1,0,1,1,0]
+    BUT_ENC_TABLE = [0,1,1,0]
 
     SLEEP_INTERVAL_S = 1
     POLLING_INTERVAL_S = 0.001
@@ -30,14 +31,22 @@ class Encoder:
         self.buttonPressedCallback = buttonPressedCallback
 
         # initialize filter and storage variables 
-        self.prevNextCode = 0
-        self.storage = 0
+        self.prevNextCodeRot = 0
+        self.storageRot = 0
+        self.prevNextCodeBut = 0
+        self.storageBut = 0
 
         # create timer and events for polling
         self.__sleepTimer = threading.Timer(self.SLEEP_INTERVAL_S, self.__stopPolling)
-        self.__pollingEvent = threading.Event()
-        self.__pollingTimer = RepeatablePausableTimer(self.POLLING_INTERVAL_S, self.__readRotation, self.__pollingEvent)
-        self.__pollingTimer.start()
+
+        self.__pollingEventRot = threading.Event()
+        self.__pollingTimerRot = RepeatablePausableTimer(self.POLLING_INTERVAL_S, self.__readRotation, self.__pollingEventRot)
+        self.__pollingTimerRot.start()
+
+        self.__pollingEventBut = threading.Event()
+        self.__pollingTimerBut = RepeatablePausableTimer(self.POLLING_INTERVAL_S, self.__readButton, self.__pollingEventBut)
+        self.__pollingEventBut.set() #debug
+        self.__pollingTimerBut.start()
 
         # set GPIO mode to board pinning
         GPIO.setmode(GPIO.BOARD)
@@ -53,7 +62,7 @@ class Encoder:
 
     def __wakeRotationPolling(self, pin):
         # set event to run polling thread
-        self.__pollingEvent.set()
+        self.__pollingEventRot.set()
 
         if(self.__sleepTimer.is_alive()):
             # user is still interacting -> reset the sleep timer
@@ -64,30 +73,51 @@ class Encoder:
             self.__sleepTimer.start()
 
     def __stopPolling(self):
-        self.__pollingEvent.clear()    # clear event to pause polling thread
+        self.__pollingEventRot.clear()    # clear event to pause polling thread
 
     # polling data from encoder including filtering and validation
     def __readRotation(self) -> None:
 
-        self.prevNextCode = self.prevNextCode << 2
+        self.prevNextCodeRot = self.prevNextCodeRot << 2
         if (GPIO.input(self.dataPin)):
-            self.prevNextCode = self.prevNextCode | 0x02
+            self.prevNextCodeRot = self.prevNextCodeRot | 0x02
         if (GPIO.input(self.clockPin)):
-            self.prevNextCode = self.prevNextCode | 0x01
-        self.prevNextCode = self.prevNextCode & 0x0f
+            self.prevNextCodeRot = self.prevNextCodeRot | 0x01
+        self.prevNextCodeRot = self.prevNextCodeRot & 0x0f
 
         # check if code is valid using table
-        if (self.ROT_ENC_TABLE[self.prevNextCode]):
-            self.storage = self.storage << 4
-            self.storage = self.storage | self.prevNextCode
+        if (self.ROT_ENC_TABLE[self.prevNextCodeRot]):
+            self.storageRot = self.storageRot << 4
+            self.storageRot = self.storageRot | self.prevNextCodeRot
 
-            if ((self.storage & 0xff) == 0x17):
+            if ((self.storageRot & 0xff) == 0x17):
                 # valid clockwise rotation
                 self.rotaryCallback(True)
 
-            if ((self.storage & 0xff) == 0x2b):
+            if ((self.storageRot & 0xff) == 0x2b):
                 # valid counterclockwise rotation
                 self.rotaryCallback(False)
 
     def __buttonPressedCallback(self, pin):
         self.buttonPressedCallback()
+
+    def __readButton(self) -> None:
+
+        self.prevNextCodeBut = self.prevNextCodeRot << 1
+        if (GPIO.input(self.buttonPin)):
+            self.prevNextCodeBut = self.prevNextCodeBut | 0x1
+        self.prevNextCodeBut = self.prevNextCodeBut & 0x3
+
+        # check if code is valid using table
+        if (self.BUT_ENC_TABLE[self.prevNextCodeBut]):
+            self.storageBut = self.storageBut << 2
+            self.storageBut = self.storageBut | self.prevNextCodeBut
+
+            if ((self.storageBut & 0xf) == 0x6):
+                # pressed and released
+                print ("pressed and released")
+                
+
+            if ((self.storageBut & 0xf) == 0x7):
+                # pressed and hold
+                print ("pressed and hold")
