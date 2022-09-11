a2e5351464a83e5ac2b1bee3da96b2c19651f582
Leo <leonhard.hesse@outlook.com>
2022-09-12 00:58:05 +0200

    test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='KY040',
-   version='1.0',
+   version='1.1',
    description='Library for encoder KY-040',
    author='Leonhard Hesse',
    author_email='service@hero-devs.de',
